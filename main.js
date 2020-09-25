a3deb4e4b3f854826ac6e49af452f40b4c5d8fa1
neeladalja14 <nadal001@plattsburgh.edu>
2020-09-25 03:57:15 -0400

    working on view more buttons for the skills menu

--- main.js
+++ main.js
@@ -29,3 +29,21 @@ $(document).ready(function () {
 
 
 });
+
+
+
+document.getElementById("open-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.remove("active");
+});
+
+document.getElementById("open-popup-btn2").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
+
+document.getElementById("open-popup-btn3").addEventListener("click",function(){
+    document.getElementsByClassName("popup")[0].classList.add("active");
+});
