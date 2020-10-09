f68afb8794da8078e5321bee8e77df9a4cd8b0b9
neeladalja14 <nadal001@plattsburgh.edu>
2020-10-08 20:04:36 -0400

    github icon was added and the twitter was removed. the gmail is not linked and directs to mail. the hover on View more changes the color on modal and its view more button. the project description has button ith animation. also they are linked to github repositories.

--- main.js
+++ main.js
@@ -31,34 +31,6 @@ $(document).ready(function () {
 });
 
 
-
-// document.getElementById("open-popup-btn").addEventListener("click",function(){
-//     document.getElementsByClassName("popup")[0].classList.add("active");
-// });
-//
-// document.getElementById("dismiss-popup-btn").addEventListener("click",function(){
-//     document.getElementsByClassName("popup")[0].classList.remove("active");
-// });
-//
-//
-//
-// document.getElementById("myopen-popup-btn2").addEventListener("click",function(){
-//     document.getElementsByClassName("mypopup2")[0].classList.add("active");
-// });
-//
-// document.getElementById("mydismiss-popup-btn2").addEventListener("click",function(){
-//     document.getElementsByClassName("mypopup2")[0].classList.remove("active");
-// });
-//
-// document.getElementById("itsopen-popup-btn").addEventListener("click",function(){
-//     document.getElementsByClassName("itspopup")[0].classList.add("active");
-// });
-//
-// document.getElementById("itsdismiss-popup-btn").addEventListener("click",function(){
-//     document.getElementsByClassName("itspopup")[0].classList.remove("active");
-// });
-
-
 var modalBtns = document.querySelectorAll('.modal-open');
 
 modalBtns.forEach(function (btn){
